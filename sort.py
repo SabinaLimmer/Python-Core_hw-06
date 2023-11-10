e924e3019e2b01fb10f67548bd365ee63b4e52c1
SabinaLimmer <kobylarz.sabina@gmail.com>
2023-11-10 20:54:21 +0100

    Update sort.py

--- sort.py
+++ sort.py
@@ -30,32 +30,34 @@ def normalize(file_name):
 # Function to process the folder and organize files
 def process_folder(folder_path):
     for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            file_name, file_extension = os.path.splitext(file)
-            normalized_file_name = normalize(file_name)
-            new_file_name = normalized_file_name + file_extension
+        categories_paths = [os.path.join(folder_path, folder) for folder in file_categories.keys()]
+        if not any(category_path in root for category_path in categories_paths):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_name, file_extension = os.path.splitext(file)
+                normalized_file_name = normalize(file_name)
+                new_file_name = normalized_file_name + file_extension
 
-            if file_extension.lower() in file_categories["images"]:
-                shutil.move(file_path, os.path.join(folder_path, 'images', new_file_name))
-            elif file_extension.lower() in file_categories["video"]:
-                shutil.move(file_path, os.path.join(folder_path, 'video', new_file_name))
-            elif file_extension.lower() in file_categories["documents"]:
-                shutil.move(file_path, os.path.join(folder_path, 'documents', new_file_name))
-            elif file_extension.lower() in file_categories["audio"]:
-                shutil.move(file_path, os.path.join(folder_path, 'audio', new_file_name))
-            elif file_extension.lower() in file_categories["archives"]:
-                shutil.move(file_path, os.path.join(folder_path, 'archives', new_file_name))
-                file_path_archives = os.path.join(folder_path, 'archives', new_file_name)
-                folder_path_archives = os.path.join(folder_path, 'archives', normalized_file_name)
-                os.makedirs(folder_path_archives, exist_ok=True)
-                shutil.unpack_archive(file_path_archives, folder_path_archives)
-            else:
-                shutil.move(file_path, os.path.join(folder_path, 'unknown', new_file_name))
+                if file_extension.lower() in file_categories["images"]:
+                    shutil.move(file_path, os.path.join(folder_path, 'images', new_file_name))
+                elif file_extension.lower() in file_categories["video"]:
+                    shutil.move(file_path, os.path.join(folder_path, 'video', new_file_name))
+                elif file_extension.lower() in file_categories["documents"]:
+                    shutil.move(file_path, os.path.join(folder_path, 'documents', new_file_name))
+                elif file_extension.lower() in file_categories["audio"]:
+                    shutil.move(file_path, os.path.join(folder_path, 'audio', new_file_name))
+                elif file_extension.lower() in file_categories["archives"]:
+                    shutil.move(file_path, os.path.join(folder_path, 'archives', new_file_name))
+                    file_path_archives = os.path.join(folder_path, 'archives', new_file_name)
+                    folder_path_archives = os.path.join(folder_path, 'archives', normalized_file_name)
+                    os.makedirs(folder_path_archives, exist_ok=True)
+                    shutil.unpack_archive(file_path_archives, folder_path_archives)
+                else:
+                    shutil.move(file_path, os.path.join(folder_path, 'unknown', new_file_name))
 
     for folder in os.listdir(folder_path):
         folder_full_path = os.path.join(folder_path, folder)
-        if os.path.isdir(folder_full_path) and folder not in file_categories.items():
+        if os.path.isdir(folder_full_path) and folder not in file_categories.keys():
             if not os.listdir(folder_full_path):
                 os.rmdir(folder_full_path)
 
